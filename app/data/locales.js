38f2f20da5ca9c11484ead2d3156d5ab85ddaea5
ebbi <ebbi40@gmail.com>
2025-12-25 20:55:07 +0700

    Data update

--- app/data/locales.js
+++ app/data/locales.js
@@ -1,7 +1,9 @@
 // app/data/locales.js
 import { loadJSON } from '../utils/fetch.js';   // tiny wrapper (see utils/fetch.js)
 
-export const SUPPORTED_LANGS = ['th', 'fa', 'zh', 'ja', 'es', 'en', 'hi', 'ar'];
+//export const SUPPORTED_LANGS = ['th', 'fa', 'zh', 'ja', 'es', 'en', 'hi', 'ar'];
+export const SUPPORTED_LANGS = ['th', 'fa', 'en'];
+
 export const FALLBACK_LANG = 'th';
 export const LS_LANG_KEY = 'local_storage_app_lang';
 
